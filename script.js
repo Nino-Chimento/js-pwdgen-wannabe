9070e21242954f16a2c034059a9f957e1f4f4132
Nino Chimento <saninoc84@gmail.com>
2019-12-17 16:51:48 +0100

    aggiunzione maledetti ;

--- script.js
+++ script.js
@@ -2,23 +2,23 @@
 var nome = prompt("come ti chiami?");
 console.log(nome);
 document.getElementById('nome-utente').innerHTML= " " + nome ;
-var cognome = prompt("qual'e' il tuo cognome?")
+var cognome = prompt("qual'e' il tuo cognome?");
 console.log(cognome);
 document.getElementById("cognome-utente").innerHTML= " " + cognome;
 var domandaSegreta = prompt("quale e' il tuo colore preferito?");
 console.log(domandaSegreta);
-document.getElementById("Domanda-segreta").innerHTML = " " + domandaSegreta
+document.getElementById("Domanda-segreta").innerHTML = " " + domandaSegreta;
 // var password= nome + cognome + domandaSegreta + "19";
 // console.log(password);
 // document.getElementById("passwordgenerata").innerHTML =" " + password;
 var numero=(prompt("Scegli un numero"));
 console.log(numero);
 var password= nome + cognome + domandaSegreta + numero;
-document.getElementById("passwordgenerata").innerHTML=" " + password;
+document.getElementById("passwordgenerata").innerHTML= " " + password;
 // bonus
 var annoDiNascita = parseInt(prompt("Qual'e l'anno di nascita tuo?"));
 
 console.log(annoDiNascita);
-var annoCorrente = 2019
+var annoCorrente = 2019;
 var laTuaEta = (annoCorrente - annoDiNascita );
-document.getElementById("tuoianni").innerHTML = laTuaEta
+document.getElementById("tuoianni").innerHTML = laTuaEta;
